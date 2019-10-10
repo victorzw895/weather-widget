0261975cf6c02970283584b55fe1b0788a9205a3
Victor Zhang <victorzw895@gmail.com>
2019-10-11 01:17:53 +1100

    https

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,7 +1,7 @@
 const _APIKEY = process.env.REACT_APP_OPEN_WEATHER_MAP;
 
 // const _APIKEY = "fdf1d48f3f3951dcb129a7e4be3b7d89";
-let apiUrl = "http://api.openweathermap.org/data/2.5";
+let apiUrl = "https://api.openweathermap.org/data/2.5";
 
 const fetchWeather = city => {
   let weeklyWeatherUrl = `${apiUrl}/weather?q=${city}&APPID=${_APIKEY}`;
